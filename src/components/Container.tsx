8dc6b24d6309d583004830ef2a9a1a822e31ed06
zh92 <zhehao92@gmail.com>
2024-02-24 16:59:35 +0800

    completed home page css

---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+export default function Container({ children }) {
+  return (
+    <div className="flex flex-col max-w-7xl min-h-screen mx-auto bg-white/[2%]">
+        {children}
+    </div>
+  )
+}
